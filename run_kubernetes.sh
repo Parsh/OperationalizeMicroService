94376b5028e4a2e23596dcf34463299d9a03c0fc
Parsh <parsh.cosmos11@gmail.com>
2020-04-06 10:24:41 +0530

    run kubernetes fillup & log

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,19 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath="parsh24/ml-microservice"
+
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl create deployment ml-microservice-kube --image=$dockerpath
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods --all-namespaces
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl expose deployment ml-microservice-kube --type="NodePort" --port=80
+kubectl port-forward deployment/ml-microservice-kube 8000:80
