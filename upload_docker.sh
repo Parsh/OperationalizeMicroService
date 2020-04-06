f8472f3441e5096de783980e5c55ae5db19df073
Parsh <parsh.cosmos11@gmail.com>
2020-04-06 09:39:17 +0530

    upload instructions

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="parsh24/ml-microservice"
+
 
 # Step 2:  
 # Authenticate & tag
+docker login --username=parsh24
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker tag ml-microservice $dockerpath
+docker push $dockerpath
